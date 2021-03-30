ad5de676267f502c14722c99c9a09ac7ffebcc0d
Christopher Yee Mon <chrisyeem@protonmail.com>
2021-03-29 20:11:37 -0700

    chapter 17 changes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -51,7 +51,8 @@ class App extends React.Component {
                         ))}
                     </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
+                {/* <Order {...this.state} /> */}
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
